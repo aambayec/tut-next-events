f3854089970199e032591a6045d0972d4e798860
Alaena Ambayec <alaena.ambayec@gmail.com>
2021-07-29 15:12:41 +0800

    comments and newsletters with mongodb

---
+++ helpers/db-util.js
@@ -0,0 +1,25 @@
+import { MongoClient } from "mongodb";
+
+export async function connectDatabase() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://Ulya:Ulyanin123@cluster0.nd8i0.mongodb.net/events?retryWrites=true&w=majority"
+  );
+
+  return client;
+}
+
+export async function insertDocument(client, collection, document) {
+  const db = client.db();
+
+  const result = await db.collection(collection).insertOne(document);
+
+  return result;
+}
+
+export async function getAllDocuments(client, collection, sort) {
+  const db = client.db();
+
+  const documents = await db.collection(collection).find().sort(sort).toArray();
+
+  return documents;
+}
